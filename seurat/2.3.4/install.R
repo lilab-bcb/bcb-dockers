52cc7326e43894f2e53ac15d91def9163fc7ade7
Yiming Yang <yang.yihming@gmail.com>
2020-03-06 15:28:42 -0500

    update installation

--- seurat/2.3.4/install.R
+++ seurat/2.3.4/install.R
@@ -1,4 +1,5 @@
 install.packages(c("devtools", "BiocManager"))
 BiocManager::install(c("S4Vectors", "made4", "SummarizedExperiment", "SingleCellExperiment", "MAST", "DESeq2", "destiny", "multtest"))
 devtools::install_github("mojaveazure/loomR", ref = "develop")
+devtools::install_version("SDMTools", version = "1.1-221.1", repos = "https://cran.r-project.org")
 devtools::install_version("Seurat", version = "2.3.4", repos = "http://cran.r-project.org", dependencies = TRUE, upgrade = "never")
